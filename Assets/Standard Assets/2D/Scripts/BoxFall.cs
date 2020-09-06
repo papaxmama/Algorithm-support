e7d86ed67aeb0c3a2d44b3cff096d340832b4dba
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-06 10:37:32 +0000

    ACPICA: Fix possible fault in return package object repair code

--- Assets/Standard Assets/2D/Scripts/BoxFall.cs
+++ Assets/Standard Assets/2D/Scripts/BoxFall.cs
@@ -1,2 +1,15 @@
 ﻿using System.Collections;
-using System.Col
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FallingBoxes
+{
+	public class BoxFall : MonoBehaviour
+	{
+
+		private Vector2 baseVelocity = new Vector2 (0f, -4f);
+		private Vector2 playerVelocity = new Vector2 (0f, -2.5f);
+
+		public void Start ()
+		{
+			this.gameObject.GetComponent<Rigidbody
