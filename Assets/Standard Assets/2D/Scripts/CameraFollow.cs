9a685a2b470d20746f5d313f4ca5cc156412cfbd
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-13 21:05:43 +0000

    PMD fixes

--- Assets/Standard Assets/2D/Scripts/CameraFollow.cs
+++ Assets/Standard Assets/2D/Scripts/CameraFollow.cs
@@ -7,4 +7,7 @@ namespace UnityStandardAssets._2D
     public class CameraFollow : MonoBehaviour
     {
         public float xMargin = 1f; // Distance in the x axis the player can move before the camera follows.
-        public float yMargin = 1f; // Distance in the y axis the playe
+        public float yMargin = 1f; // Distance in the y axis the player can move before the camera follows.
+        public float xSmooth = 8f; // How smoothly the camera catches up with it's target movement in the x axis.
+        public float ySmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis.
+        public Vector2 maxXAndY; // The maximum x and y coordin
