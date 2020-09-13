da30984b6beda7efe02d784bdd075908e91a9fcb
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-13 19:12:07 +0000

    Update development

--- Assets/Standard Assets/2D/Scripts/CameraFollow.cs
+++ Assets/Standard Assets/2D/Scripts/CameraFollow.cs
@@ -1,1 +1,10 @@
-using
+using System;
+using UnityEngine;
+
+
+namespace UnityStandardAssets._2D
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public float xMargin = 1f; // Distance in the x axis the player can move before the camera follows.
+        public float yMargin = 1f; // Distance in the y axis the playe
