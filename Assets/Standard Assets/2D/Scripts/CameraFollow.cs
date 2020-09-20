ef2558f26ac51e6b90db72c1af3081548c5f7195
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-20 10:57:46 +0000

    Linux 3.0.45

--- Assets/Standard Assets/2D/Scripts/CameraFollow.cs
+++ Assets/Standard Assets/2D/Scripts/CameraFollow.cs
@@ -10,4 +10,13 @@ namespace UnityStandardAssets._2D
         public float yMargin = 1f; // Distance in the y axis the player can move before the camera follows.
         public float xSmooth = 8f; // How smoothly the camera catches up with it's target movement in the x axis.
         public float ySmooth = 8f; // How smoothly the camera catches up with it's target movement in the y axis.
-        public Vector2 maxXAndY; // The maximum x and y coordin
+        public Vector2 maxXAndY; // The maximum x and y coordinates the camera can have.
+        public Vector2 minXAndY; // The minimum x and y coordinates the camera can have.
+
+        private Transform m_Player; // Reference to the player's transform.
+
+
+        private void Awake()
+        {
+            // Setting up the reference.
+            m_Player = GameObject.FindGameObjectWithTag("Play
