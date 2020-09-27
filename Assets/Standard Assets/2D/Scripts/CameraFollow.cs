196e1370c35baf60b0bd260633eade0309e0fb44
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-27 13:08:58 +0000

    Release 0.5.0

--- Assets/Standard Assets/2D/Scripts/CameraFollow.cs
+++ Assets/Standard Assets/2D/Scripts/CameraFollow.cs
@@ -65,4 +65,10 @@ namespace UnityStandardAssets._2D
 
             // The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
             targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
-            targetY = Mathf.Clamp(targetY, minXAndY.y, 
+            targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+
+            // Set the camera's position to the target position with the same z component.
+            transform.position = new Vector3(targetX, targetY, transform.position.z);
+        }
+    }
+}
