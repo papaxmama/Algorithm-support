cd2813e148c4b5a061f9854b6a86f365b9a3b74a
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-27 09:17:45 +0000

    DB/Misc: Fix startup errors

--- Assets/Standard Assets/2D/Scripts/CameraFollow.cs
+++ Assets/Standard Assets/2D/Scripts/CameraFollow.cs
@@ -60,4 +60,9 @@ namespace UnityStandardAssets._2D
             if (CheckYMargin())
             {
                 // ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
-                targetY = Mathf.Ler
+                targetY = Mathf.Lerp(transform.position.y, m_Player.position.y, ySmooth*Time.deltaTime);
+            }
+
+            // The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
+            targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+            targetY = Mathf.Clamp(targetY, minXAndY.y, 
