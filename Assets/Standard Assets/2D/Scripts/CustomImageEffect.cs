7742f17f665724afeebbbc146f2bcb89582e1d14
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-09-27 18:24:57 +0000

    revup

--- Assets/Standard Assets/2D/Scripts/CustomImageEffect.cs
+++ Assets/Standard Assets/2D/Scripts/CustomImageEffect.cs
@@ -5,4 +5,12 @@ using System.Collections.Generic;
 public class CustomImageEffect : MonoBehaviour
 {
 	public Material EffectMaterial;
-	public List<Vect
+	public List<Vector3> lasers;
+	private Camera myCamera;
+
+	void OnRenderImage (RenderTexture src, RenderTexture dst)
+	{
+		RenderTexture tmp = RenderTexture.GetTemporary (src.width, src.height);
+		Graphics.Blit (src, tmp);
+		foreach (Vector3 ray in lasers) {
+			RenderTexture tmp2 = RenderTexture.GetTemporary (tmp.width,
