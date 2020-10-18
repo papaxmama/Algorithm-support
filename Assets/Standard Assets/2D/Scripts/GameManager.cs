2c820b1fe0c30f382aacdf849ee1092f4d48ceda
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-10-18 22:41:33 +0000

    drm/i915: Unpin the flip target if we fail to queue the flip

--- Assets/Standard Assets/2D/Scripts/GameManager.cs
+++ Assets/Standard Assets/2D/Scripts/GameManager.cs
@@ -22,4 +22,18 @@ namespace FallingBoxes
 		// public float maxGravity = 1.25f;
 		private int difficulty = 1;
 		private int highestDifficulty = 1;
-		private
+		private int numLasers = 0;
+		private int boxDeltaX = 10;
+		private int laserDeltaX = 14;
+		public int maxTotalLasers = 5;
+		private int minArea = 2;
+
+		void Awake ()
+		{
+			InitGame ();
+		}
+
+		// Update is called once per frame
+		void Update ()
+		{
+			difficulty = ((int)Mathf.Abs (character.trans
