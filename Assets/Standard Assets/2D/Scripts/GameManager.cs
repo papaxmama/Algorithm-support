93777d843800ea056a0b43a7b19bcc4bef9925a5
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-10-18 14:33:51 +0000

    Reset version number

--- Assets/Standard Assets/2D/Scripts/GameManager.cs
+++ Assets/Standard Assets/2D/Scripts/GameManager.cs
@@ -15,4 +15,11 @@ namespace FallingBoxes
 		private Transform gameManager;
 		private int boxSpawnPeriodicity = 400;
 		private int laserSpawnPeriodicity = 50;
-		
+		private static float maxHeightReached = 0f;
+		private int boxSpawnOffset = 40;
+		private int boxSpawnVariance = 0;
+		// public float minGravity = 0.75f;
+		// public float maxGravity = 1.25f;
+		private int difficulty = 1;
+		private int highestDifficulty = 1;
+		private
