5fff4a4b87a79d023e81fb12dbc600bf8c6033ed
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-10-18 06:09:39 +0000

    Tests refactoring

--- Assets/Standard Assets/2D/Scripts/GameManager.cs
+++ Assets/Standard Assets/2D/Scripts/GameManager.cs
@@ -4,4 +4,15 @@ using UnityEngine.UI;
 namespace FallingBoxes
 {
 
-	public class GameManager : MonoBeha
+	public class GameManager : MonoBehaviour
+	{
+
+		public GameObject box;
+		public GameObject platformPiece;
+		public GameObject character;
+		public GameObject laser;
+		public GameObject motivationArrow;
+		private Transform gameManager;
+		private int boxSpawnPeriodicity = 400;
+		private int laserSpawnPeriodicity = 50;
+		
