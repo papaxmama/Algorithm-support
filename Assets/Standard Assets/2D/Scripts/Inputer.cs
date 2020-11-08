2b1f96c10526d89b826d6610b8689b635f138e2d
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-11-08 18:44:38 +0000

    Add lockfile

--- Assets/Standard Assets/2D/Scripts/Inputer.cs
+++ Assets/Standard Assets/2D/Scripts/Inputer.cs
@@ -11,4 +11,17 @@ namespace FallingBoxes
 		private MainCharacter m_Character;
 		private bool m_Jump;
 
-		priva
+		private void Awake ()
+		{
+			m_Character = GetComponent<MainCharacter> ();
+		}
+
+
+		private void Update ()
+		{
+			if (!m_Jump) {
+				// Read the jump input in Update so button presses aren't missed.
+				m_Jump = CrossPlatformInputManager.GetButtonDown ("Jump");
+			}
+
+			if (CrossPlatformInputManager.GetButto
