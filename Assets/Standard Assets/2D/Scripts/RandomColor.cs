5c719c212f84a5f70a5f8c0b7b073a07f7a3951a
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-11-11 22:29:37 +0000

    Random stuff

---
+++ Assets/Standard Assets/2D/Scripts/RandomColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour {
+
+    Material material;
+
+    // Use this for initialization
