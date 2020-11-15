df77cf791d3fdfc6799da4cb560e998ff5017c06
papaxmama <121256110+papaxmama@users.noreply.github.com>
2020-11-15 05:28:41 +0000

    ^^

--- Assets/Standard Assets/2D/Scripts/RandomColor.cs
+++ Assets/Standard Assets/2D/Scripts/RandomColor.cs
@@ -7,3 +7,15 @@ public class RandomColor : MonoBehaviour {
     Material material;
 
     // Use this for initialization
+    void Start () {
+	material = GetComponent<Renderer>().material;
+	// setRandomRGB();
+	setRGB(.5f, .5f, .5f);
+    }
+
+    private void setRandomRGB(){
+	setRGB(UnityEngine.Random.value,
+	       UnityEngine.Random.value,
+	       UnityEngine.Random.value);
+    }
+    private void setRGB(float r, fl
