931728a83c49d8641b572c9338430c031c4a9f19
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-06-17 10:42:08 +0000

    cpufreq: Introduce new relation for freq selection

---
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+	public class Joystick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+	{
+		public enum AxisOption
+		{
+			// Options for which axes to use
+			Both, // Use both
+			OnlyHorizonta
