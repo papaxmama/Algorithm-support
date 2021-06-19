f8407a21d2d67471d40dc893107108f883e93f3b
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-06-19 13:50:57 +0000

    6.9.0

--- Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
@@ -16,4 +16,10 @@ namespace UnityStandardAssets.CrossPlatformInput
 
 		public int MovementRange = 100;
 		public AxisOption axesToUse = AxisOption.Both; // The options for the axes that the still will use
-		public string horizontalAxisName = "Horizontal"; // The name given to the horizontal axis for the cross platf
+		public string horizontalAxisName = "Horizontal"; // The name given to the horizontal axis for the cross platform input
+		public string verticalAxisName = "Vertical"; // The name given to the vertical axis for the cross platform input
+
+		Vector3 m_StartPos;
+		bool m_UseX; // Toggle for using the x axis
+		bool m_UseY; // Toggle for using the Y axis
+		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to 
