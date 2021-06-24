030768be57d9ad5c569e530dbb0e454b1f7b4f03
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-06-24 08:01:05 +0000

    minor format

--- Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
@@ -22,4 +22,17 @@ namespace UnityStandardAssets.CrossPlatformInput
 		Vector3 m_StartPos;
 		bool m_UseX; // Toggle for using the x axis
 		bool m_UseY; // Toggle for using the Y axis
-		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to 
+		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to the joystick in the cross platform input
+		CrossPlatformInputManager.VirtualAxis m_VerticalVirtualAxis; // Reference to the joystick in the cross platform input
+
+		void OnEnable()
+		{
+			CreateVirtualAxes();
+		}
+
+        void Start()
+        {
+            m_StartPos = transform.position;
+        }
+
+		void UpdateVirtualAxes(V
