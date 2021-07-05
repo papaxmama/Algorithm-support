fb4057fdbb8e81d830d93865414cd49f4da00768
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-07-05 06:11:49 +0000

    update javascript

--- Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
@@ -1,2 +1,14 @@
 using System;
-#if UNITY_EDIT
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+    [ExecuteInEditMode]
+    public class MobileControlRig : MonoBehaviour
+    {
+        // this script enables or disables the child objects of a control rig
+        // depending on whether
