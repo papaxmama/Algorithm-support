57b15e7fdade3e7b5a06f8060b93798e71bdea72
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-07-05 13:11:14 +0000

    prepublish

--- Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
@@ -11,4 +11,19 @@ namespace UnityStandardAssets.CrossPlatformInput
     public class MobileControlRig : MonoBehaviour
     {
         // this script enables or disables the child objects of a control rig
-        // depending on whether
+        // depending on whether the USE_MOBILE_INPUT define is declared.
+
+        // This define is set or unset by a menu item that is included with
+        // the Cross Platform Input package.
+
+#if !UNITY_EDITOR
+	void OnEnable()
+	{
+		CheckEnableControlRig();
+	}
+	#endif
+
+        private void Start()
+        {
+#if UNITY_EDITOR
+            i
