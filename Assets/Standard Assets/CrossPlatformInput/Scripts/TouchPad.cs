092d03195ec6e21c29c836d9f12a231ccfeaab59
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-07-23 20:14:56 +0000

    Update zaobao rss...

--- Assets/Standard Assets/CrossPlatformInput/Scripts/TouchPad.cs
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/TouchPad.cs
@@ -41,3 +41,117 @@ namespace UnityStandardAssets.CrossPlatformInput
 		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to the joystick in the cross platform input
 		CrossPlatformInputManager.VirtualAxis m_VerticalVirtualAxis; // Reference to the joystick in the cross platform input
 		bool m_Dragging;
+		int m_Id = -1;
+		Vector2 m_PreviousTouchPos; // swipe style control touch
+
+
+#if !UNITY_EDITOR
+    private Vector3 m_Center;
+    private Image m_Image;
+#else
+		Vector3 m_PreviousMouse;
+#endif
+
+		void OnEnable()
+		{
+			CreateVirtualAxes();
+		}
+
+        void Start()
+        {
+#if !UNITY_EDITOR
+            m_Image = GetComponent<Image>();
+            m_Center = m_Image.transform.position;
+#endif
+        }
+
+		void CreateVirtualAxes()
+		{
+			// set axes to use
+			m_UseX = (axesToUse == AxisOption.Both || axesToUse == AxisOption.OnlyHorizontal);
+			m_UseY = (axesToUse == AxisOption.Both || axesToUse == AxisOption.OnlyVertical);
+
+			// create new axes based on axes to use
+			if (m_UseX)
+			{
+				m_HorizontalVirtualAxis = new CrossPlatformInputManager.VirtualAxis(horizontalAxisName);
+				CrossPlatformInputManager.RegisterVirtualAxis(m_HorizontalVirtualAxis);
+			}
+			if (m_UseY)
+			{
+				m_VerticalVirtualAxis = new CrossPlatformInputManager.VirtualAxis(verticalAxisName);
+				CrossPlatformInputManager.RegisterVirtualAxis(m_VerticalVirtualAxis);
+			}
+		}
+
+		void UpdateVirtualAxes(Vector3 value)
+		{
+			value = value.normalized;
+			if (m_UseX)
+			{
+				m_HorizontalVirtualAxis.Update(value.x);
+			}
+
+			if (m_UseY)
+			{
+				m_VerticalVirtualAxis.Update(value.y);
+			}
+		}
+
+
+		public void OnPointerDown(PointerEventData data)
+		{
+			m_Dragging = true;
+			m_Id = data.pointerId;
+#if !UNITY_EDITOR
+        if (controlStyle != ControlStyle.Absolute )
+            m_Center = data.position;
+#endif
+		}
+
+		void Update()
+		{
+			if (!m_Dragging)
+			{
+				return;
+			}
+			if (Input.touchCount >= m_Id + 1 && m_Id != -1)
+			{
+#if !UNITY_EDITOR
+
+            if (controlStyle == ControlStyle.Swipe)
+            {
+                m_Center = m_PreviousTouchPos;
+                m_PreviousTouchPos = Input.touches[m_Id].position;
+            }
+            Vector2 pointerDelta = new Vector2(Input.touches[m_Id].position.x - m_Center.x , Input.touches[m_Id].position.y - m_Center.y).normalized;
+            pointerDelta.x *= Xsensitivity;
+            pointerDelta.y *= Ysensitivity;
+#else
+				Vector2 pointerDelta;
+				pointerDelta.x = Input.mousePosition.x - m_PreviousMouse.x;
+				pointerDelta.y = Input.mousePosition.y - m_PreviousMouse.y;
+				m_PreviousMouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+#endif
+				UpdateVirtualAxes(new Vector3(pointerDelta.x, pointerDelta.y, 0));
+			}
+		}
+
+
+		public void OnPointerUp(PointerEventData data)
+		{
+			m_Dragging = false;
+			m_Id = -1;
+			UpdateVirtualAxes(Vector3.zero);
+		}
+
+		void OnDisable()
+		{
+			if (CrossPlatformInputManager.AxisExists(horizontalAxisName))
+				CrossPlatformInputManager.UnRegisterVirtualAxis(horizontalAxisName);
+
+			if (CrossPlatformInputManager.AxisExists(verticalAxisName))
+				CrossPlatformInputManager.UnRegisterVirtualAxis(verticalAxisName);
+		}
+	}
+}
---
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/VirtualInput.cs
@@ -0,0 +1,13 @@
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UnityStandardAssets.CrossPlatformInput
+{
+    public abstract class VirtualInput
+    {
+        public Vector3 virtualMousePosition { get; private set; }
+        
+        
