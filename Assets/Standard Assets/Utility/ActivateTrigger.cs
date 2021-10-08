46dfcc6d81ae12e933bc5ac5a64c6b5f4fced40a
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-10-08 14:30:38 +0000

    Methods

--- Assets/Standard Assets/Utility/ActivateTrigger.cs
+++ Assets/Standard Assets/Utility/ActivateTrigger.cs
@@ -1,3 +1,13 @@
 using System;
 using UnityEngine;
-using Object 
+using Object = UnityEngine.Object;
+
+namespace UnityStandardAssets.Utility
+{
+    public class ActivateTrigger : MonoBehaviour
+    {
+        // A multi-purpose script which causes an action to occur when
+        // a trigger collider is entered.
+        public enum Mode
+        {
+            Trigger =
