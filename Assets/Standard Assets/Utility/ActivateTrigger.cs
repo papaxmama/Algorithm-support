bdf975f1de8298a8576bc94decf7cd808b6341b9
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-10-13 17:53:49 +0000

    Updated build.

--- Assets/Standard Assets/Utility/ActivateTrigger.cs
+++ Assets/Standard Assets/Utility/ActivateTrigger.cs
@@ -10,4 +10,9 @@ namespace UnityStandardAssets.Utility
         // a trigger collider is entered.
         public enum Mode
         {
-            Trigger =
+            Trigger = 0,    // Just broadcast the action on to the target
+            Replace = 1,    // replace target with source
+            Activate = 2,   // Activate the target GameObject
+            Enable = 3,     // Enable a component
+            Animate = 4,    // Start animation on target
+            Deactivate = 
