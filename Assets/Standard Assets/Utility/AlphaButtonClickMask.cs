e029932cda5c089dd19a298fb8cc1d82859093b0
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-10-26 11:18:47 +0000

    removed todos

--- Assets/Standard Assets/Utility/AlphaButtonClickMask.cs
+++ Assets/Standard Assets/Utility/AlphaButtonClickMask.cs
@@ -1,3 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.C
+using System.Collections;
+
+public class AlphaButtonClickMask : MonoBehaviour, ICanvasRaycastFilter 
+{
+    protected Image _image;
+
+    public void Start()
+    {
+        _image = GetComponent<Image>();
+
+        Texture2D tex = _image.sprite.texture as Texture2D;
+
+        bool isInvalid = false;
+        if (tex != null)
+      
