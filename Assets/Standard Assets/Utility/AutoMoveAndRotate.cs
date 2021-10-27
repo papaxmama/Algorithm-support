e63ca01d94fb751a70c645e52cbc3cdcdc841391
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-10-27 08:01:35 +0000

    v0.4.6

--- Assets/Standard Assets/Utility/AutoMoveAndRotate.cs
+++ Assets/Standard Assets/Utility/AutoMoveAndRotate.cs
@@ -6,4 +6,18 @@ namespace UnityStandardAssets.Utility
     public class AutoMoveAndRotate : MonoBehaviour
     {
         public Vector3andSpace moveUnitsPerSecond;
-        p
+        public Vector3andSpace rotateDegreesPerSecond;
+        public bool ignoreTimescale;
+        private float m_LastRealTime;
+
+
+        private void Start()
+        {
+            m_LastRealTime = Time.realtimeSinceStartup;
+        }
+
+
+        // Update is called once per frame
+        private void Update()
+        {
+    
