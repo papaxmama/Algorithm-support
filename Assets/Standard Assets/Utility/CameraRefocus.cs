0bc77192bd76a15b53dd438216ce6e9866ce443a
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-10-27 13:30:50 +0000

    adding rspec

--- Assets/Standard Assets/Utility/CameraRefocus.cs
+++ Assets/Standard Assets/Utility/CameraRefocus.cs
@@ -3,4 +3,16 @@ using UnityEngine;
 
 namespace UnityStandardAssets.Utility
 {
-    public class CameraR
+    public class CameraRefocus
+    {
+        public Camera Camera;
+        public Vector3 Lookatpoint;
+        public Transform Parent;
+
+        private Vector3 m_OrigCameraPos;
+        private bool m_Refocus;
+
+
+        public CameraRefocus(Camera camera, Transform parent, Vector3 origCameraPos)
+        {
+          
