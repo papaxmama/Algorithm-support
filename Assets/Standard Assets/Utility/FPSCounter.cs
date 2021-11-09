b2c2ba82343b1d7ed887967d7f89666f3cf9e8a0
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-11-09 10:39:57 +0000

    added basic readme

--- Assets/Standard Assets/Utility/FPSCounter.cs
+++ Assets/Standard Assets/Utility/FPSCounter.cs
@@ -4,4 +4,15 @@ using UnityEngine.UI;
 
 namespace UnityStandardAssets.Utility
 {
-    [RequireComponent(typeof (Text))
+    [RequireComponent(typeof (Text))]
+    public class FPSCounter : MonoBehaviour
+    {
+        const float fpsMeasurePeriod = 0.5f;
+        private int m_FpsAccumulator = 0;
+        private float m_FpsNextPeriod = 0;
+        private int m_CurrentFps;
+        const string display = "{0} FPS";
+        private Text m_Text;
+
+
+     
