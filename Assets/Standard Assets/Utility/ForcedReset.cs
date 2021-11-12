b1a0b335216480352c637e8e852dec4230700ea4
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-11-12 18:30:13 +0000

    style update

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,4 +1,15 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityStandardAssets.CrossPlat
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (GUITexture))]
+public class ForcedReset : MonoBehaviour
+{
+    private void Update()
+    {
+        // if we have forced a reset ...
+        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+        {
+            //... reload the scene
+            SceneManager.LoadScene(SceneMana
