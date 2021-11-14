c53b229f0616b05d82e86bc96f89f2e4d3a433eb
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-11-14 18:23:24 +0000

    gifs

--- Assets/Standard Assets/Utility/LerpControlledBob.cs
+++ Assets/Standard Assets/Utility/LerpControlledBob.cs
@@ -1,3 +1,20 @@
 using System;
 using System.Collections;
 using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+    [Serializable]
+    public class LerpControlledBob
+    {
+        public float BobDuration;
+        public float BobAmount;
+
+        private float m_Offset = 0f;
+
+
+        // provides the offset that can be used
+        public float Offset()
+        {
+            return m_Offset;
+ 
