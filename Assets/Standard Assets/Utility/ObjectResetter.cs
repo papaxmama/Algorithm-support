c64f3353cb154200038a56a8e22a91970222836c
papaxmama <121256110+papaxmama@users.noreply.github.com>
2021-12-16 18:50:08 +0000

    Release 3.2

--- Assets/Standard Assets/Utility/ObjectResetter.cs
+++ Assets/Standard Assets/Utility/ObjectResetter.cs
@@ -1,2 +1,12 @@
 using System;
-using Syste
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+    public class ObjectResetter : MonoBehaviour
+    {
+        private Vector3 originalPosition;
+        private Quaternion originalRotation;
+        private List<Transform> originalStruc
