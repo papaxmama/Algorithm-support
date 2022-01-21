7d692cf9b2a3d6e4f42e1d50593417d2d9c60eba
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-01-21 06:01:35 +0000

    Update application.html.erb

--- Assets/Standard Assets/Utility/ParticleSystemDestroyer.cs
+++ Assets/Standard Assets/Utility/ParticleSystemDestroyer.cs
@@ -1,4 +1,12 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using Random = UnityEng
+using Random = UnityEngine.Random;
+
+namespace UnityStandardAssets.Utility
+{
+    public class ParticleSystemDestroyer : MonoBehaviour
+    {
+        // allows a particle system to exist for a specified duration,
+        // then shuts off emission, and waits for all particles to expire
+        // before destroying the gameObject
