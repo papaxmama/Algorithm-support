9d67ace25d9f7963eeba398171502280851709e6
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-03-23 06:05:33 +0000

    Upload js

---
+++ Assets/Standard Assets/Utility/SimpleMouseRotator.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Utility
+{
+    public class SimpleMouseRotator : MonoBehaviour
+    {
+        // A mouselook behaviour with
