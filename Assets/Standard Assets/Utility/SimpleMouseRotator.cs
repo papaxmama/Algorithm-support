8dc0d18854ce65d041c6d83f10450438629e551f
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-03-23 06:29:43 +0000

    cleaned a bit

--- Assets/Standard Assets/Utility/SimpleMouseRotator.cs
+++ Assets/Standard Assets/Utility/SimpleMouseRotator.cs
@@ -6,4 +6,10 @@ namespace UnityStandardAssets.Utility
 {
     public class SimpleMouseRotator : MonoBehaviour
     {
-        // A mouselook behaviour with
+        // A mouselook behaviour with constraints which operate relative to
+        // this gameobject's initial rotation.
+        // Only rotates around local X and Y.
+        // Works in local coordinates, so if this object is parented
+        // to another moving gameobject, its local constraints will
+        // operate correctly
+        /
