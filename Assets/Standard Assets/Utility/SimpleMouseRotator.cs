3395f9b6305b5c2a656175b347370e8cad49612e
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-05-03 07:51:51 +0000

    xen/m2p: do not reuse kmap_op->dev_bus_addr

--- Assets/Standard Assets/Utility/SimpleMouseRotator.cs
+++ Assets/Standard Assets/Utility/SimpleMouseRotator.cs
@@ -72,4 +72,11 @@ namespace UnityStandardAssets.Utility
 
 #if MOBILE_INPUT
             // on mobile, sometimes we want input mapped directly to tilt value,
-            // so it s
+            // so it springs back automatically when the look input is released.
+			if (autoZeroHorizontalOnMobile) {
+				m_TargetAngles.y = Mathf.Lerp (-rotationRange.y * 0.5f, rotationRange.y * 0.5f, inputH * .5f + .5f);
+			} else {
+				m_TargetAngles.y += inputH * rotationSpeed;
+			}
+			if (autoZeroVerticalOnMobile) {
+				m_TargetAngles.x = 
