59aac63f01ec2db7ba17a5a212504185e1118a5e
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-06-06 06:01:27 +0000

    stylistic changes

---
+++ Assets/Standard Assets/Utility/WaypointCircuit.cs
@@ -0,0 +1,128 @@
+
+using System;
+using System.Collections;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace UnityStandardAssets.Utility
+{
+    public class WaypointCircuit : MonoBehaviour
+    {
+        public WaypointList waypointList = new WaypointList();
+        [SerializeField] private bool smoothRoute = true;
+        private int numPoints;
+        private Vector3[] points;
+        private float[] distances;
+
+        public float editorVisualisationSubsteps = 100;
+        public float Length { get; private set; }
+
+        public Transform[] Waypoints
+        {
+            get { return waypointList.items; }
+        }
+
+        //this being here will save GC allocs
+        private int p0n;
+        private int p1n;
+        private int p2n;
+        private int p3n;
+
+        private float i;
+        private Vector3 P0;
+        private Vector3 P1;
+        private Vector3 P2;
+        private Vector3 P3;
+
+        // Use this for initialization
+        private void Awake()
+        {
+            if (Waypoints.Length > 1)
+            {
+                CachePositionsAndDistances();
+            }
+            numPoints = Waypoints.Length;
+        }
+
+
+        public RoutePoint GetRoutePoint(float dist)
+        {
+            // position and direction
+            Vector3 p1 = GetRoutePosition(dist);
+            Vector3 p2 = GetRoutePosition(dist + 0.1f);
+            Vector3 delta = p2 - p1;
+            return new RoutePoint(p1, delta.normalized);
+        }
+
+
+        public Vector3 GetRoutePosition(float dist)
+        {
+            int point = 0;
+
+            if (Length == 0)
+            {
+                Length = distances[distances.Length - 1];
+            }
+
+            dist = Mathf.Repeat(dist, Length);
+
+            while (distances[point] < dist)
+            {
+                ++point;
+            }
+
+
+            // get nearest two points, ensuring points wrap-around start & end of circuit
+            p1n = ((point - 1) + numPoints)%numPoints;
+            p2n = point;
+
+            // found point numbers, now find interpolation value between the two middle points
+
+            i = Mathf.InverseLerp(distances[p1n], distances[p2n], dist);
+
+            if (smoothRoute)
+            {
+                // smooth catmull-rom calculation between the two relevant points
+
+
+                // get indices for the surrounding 2 points, because
+                // four points are required by the catmull-rom function
+                p0n = ((point - 2) + numPoints)%numPoints;
+                p3n = (point + 1)%numPoints;
+
+                // 2nd point may have been the 'last' point - a dupe of the first,
+                // (to give a value of max track distance instead of zero)
+                // but now it must be wrapped back to zero if that was the case.
+                p2n = p2n%numPoints;
+
+                P0 = points[p0n];
+                P1 = points[p1n];
+                P2 = points[p2n];
+                P3 = points[p3n];
+
+                return CatmullRom(P0, P1, P2, P3, i);
+            }
+            else
+            {
+                // simple linear lerp between the two points:
+
+                p1n = ((point - 1) + numPoints)%numPoints;
+                p2n = point;
+
+                return Vector3.Lerp(points[p1n], points[p2n], i);
+            }
+        }
+
+
+        private Vector3 CatmullRom(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float i)
+        {
+            // comments are no use here... it's the catmull-rom equation.
+            // Un-magic this, lord vector!
+            return 0.5f*
+                   ((2*p1) + (-p0 + p2)*i + (2*p0 - 5*p1 + 4*p2 - p3)*i*i +
+                    (-p0 + 3*p1 - 3*p2 + p3)*i*i*i);
+        }
+
