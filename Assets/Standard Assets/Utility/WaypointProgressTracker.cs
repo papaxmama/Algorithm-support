17d00d43ca95b2654caf10cc55fc6160376194c8
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-06-08 22:05:13 +0000

    resume updated

---
+++ Assets/Standard Assets/Utility/WaypointProgressTracker.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+    public class WaypointProgressTracker : MonoBehaviour
+    {
+        // This script can be used with any object that
