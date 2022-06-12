d6bb8d6610b00cc7b0a7ee8ba5e61e19a6c13647
papaxmama <121256110+papaxmama@users.noreply.github.com>
2022-06-12 06:15:26 +0000

    pull from iogrow

--- Assets/Standard Assets/Utility/WaypointProgressTracker.cs
+++ Assets/Standard Assets/Utility/WaypointProgressTracker.cs
@@ -5,4 +5,10 @@ namespace UnityStandardAssets.Utility
 {
     public class WaypointProgressTracker : MonoBehaviour
     {
-        // This script can be used with any object that
+        // This script can be used with any object that is supposed to follow a
+        // route marked out by waypoints.
+
+        // This script manages the amount to look ahead along the route,
+        // and keeps track of progress and laps.
+
+        [SerializeField] private WaypointCircuit circuit; // A reference to the waypoint-based route we should fol
